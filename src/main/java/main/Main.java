7b912444d48aedf5985dc6336fb7fc44d9e2ed12
Augusto <senescal.oliveira@gmail.com>
2025-03-08 09:06:54 -0300

    feat: adicionado classe jogador e formato de exibição do objeto

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import model.Jogador;
+
+public class Main {
+    public static void main(String[] args){
+
+        Jogador jogador1 = new Jogador("Maria", 0);
+        Jogador jogador2 = new Jogador("João", 0);
+        Jogador jogador3 = new Jogador("Pedro", 0);
+
+        System.out.println(jogador1);
+
+    }
+}
---
+++ src/main/java/model/Jogador.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Jogador {
+    private String nome;
+    private int pontuacao;
+
+    public Jogador(String nome, int pontuacao) {
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Jogador: %s | Pontuação: %d", this.nome, this.pontuacao);
+    }
+}
